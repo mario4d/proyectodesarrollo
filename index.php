cb47a3b2fd9ef6d762a36c6f5c377d2f39edc0d8
mario4d <mduque141@gmail.com>
2024-10-10 20:44:46 -0500

    cambio de diseño

--- index.php
+++ index.php
@@ -1,62 +1,61 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulario de Producto</title>
-    <link rel="stylesheet" href="css/estilo.css">                  
+    <link rel="stylesheet" href="css/estilo.css"> 
 </head>
 <body>
+    <nav>
+        <ul>
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Acerca de</a></li>
+            <li><a href="#">Servicios</a></li>
+            <li><a href="#">Contacto</a></li>
+        </ul>
+    </nav>
+
     <div class="container">
         <h2>Añadir producto</h2>
         
         <form action="Operaciones.php" method="POST">
-            <table>
-                <thead>
-                    <tr>
-                        <th>Código</th>
-                        <td><input type="number" name="id" placeholder="Ingrese el ID del producto" maxlength="7" required></td>
-                    </tr>
-                    <tr>
-                        <th>Nombre del Producto</th>
-                        <td><input type="text" name="nombre" placeholder="Ingrese el nombre del producto" required></td>
-                    </tr>
-                    <tr>
-                        <th>Descripción del producto</th>
-                        <td><input type="text" name="detalle" placeholder="Ingrese los detalles del producto" required></td>
-                    </tr>
-                    <tr>
-                        <th>Precio del producto</th>
-                        <td><input type="number" name="precio" placeholder="Ingrese el precio" required></td>
-                    </tr>
-                   
-                    <tr>
-                        <th>Stock</th>
-                        <td><input type="text" name="nproveedor" placeholder="Ingrese el nombre del proveedor" required></td>
-                    </tr>
-                </thead>
-            </table>
-
-            <table>
-                <thead>
-                    <tr>
-                        <th><label>ID proveedor</label></th>
-                        <td><input type="number" name="id_proveedor" placeholder="Ingrese el ID del proveedor" required></td>
-                    </tr>
-                    <tr>
-                        <th><label>Nombre de proveedor</label></th>
-                        <td><input type="text" name="nombre_proveedor" placeholder="Ingrese el nombre del proveedor" required></td>
-                    </tr>
-                    <tr>
-                        <th><label>Teléfono del proveedor</label></th>
-                        <td><input type="number" name="telefono_proveedor" placeholder="Ingrese el teléfono del proveedor" required></td>
-                    </tr>
-                    <tr>
-                        <th><label>Email del proveedor</label></th>
-                        <td><input type="email" name="email_proveedor" placeholder="Ingrese el email del proveedor" required></td>
-                    </tr>
-                </thead> 
-            </table>
+            <div class="form-group">
+                <label>Código</label>
+                <input type="number" name="id" placeholder="Ingrese el ID del producto" maxlength="7" required>
+            </div>
+            <div class="form-group">
+                <label>Nombre del Producto</label>
+                <input type="text" name="nombre" placeholder="Ingrese el nombre del producto" required>
+            </div>
+            <div class="form-group">
+                <label>Descripción del producto</label>
+                <input type="text" name="detalle" placeholder="Ingrese los detalles del producto" required>
+            </div>
+            <div class="form-group">
+                <label>Precio del producto</label>
+                <input type="number" name="precio" placeholder="Ingrese el precio" required>
+            </div>
+            <div class="form-group">
+                <label>Stock</label>
+                <input type="text" name="nproveedor" placeholder="Ingrese el nombre del proveedor" required>
+            </div>
+            <div class="form-group">
+                <label>ID proveedor</label>
+                <input type="number" name="id_proveedor" placeholder="Ingrese el ID del proveedor" required>
+            </div>
+            <div class="form-group">
+                <label>Nombre de proveedor</label>
+                <input type="text" name="nombre_proveedor" placeholder="Ingrese el nombre del proveedor" required>
+            </div>
+            <div class="form-group">
+                <label>Teléfono del proveedor</label>
+                <input type="number" name="telefono_proveedor" placeholder="Ingrese el teléfono del proveedor" required>
+            </div>
+            <div class="form-group">
+                <label>Email del proveedor</label>
+                <input type="email" name="email_proveedor" placeholder="Ingrese el email del proveedor" required>
+            </div>
 
             <div class="text-center">
                 <button type="submit" class="btn btn-primary" name="agregar">Guardar</button>
@@ -68,7 +67,7 @@
         </form>
 
         <div>
-            <table border="1" width="100%">
+            <table>
                 <thead>
                     <tr>
                         <th>Código</th>
